18ebcd5e33f0ffad82cb31716bc1cb2b0332bcc8
Timo Vesalainen <timovesalainen@hotmail.com>
2024-07-09 20:57:07 +0300

    Added test projects and basic combat test

---
+++ EclipseCombatCalculatorLibraryTest/CombatTest.cs
@@ -0,0 +1,35 @@
+namespace EclipseCombatCalculatorLibraryTest
+{
+    public class CombatTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private class TestShip : IShipStats
+        {
+            public int Initiative { get; set; }
+
+            public Dice[]? Weapons { get; set; }
+
+            public int Computers { get; set; }
+
+            public int Shields { get; set; }
+
+            public int Hulls { get; set; }
+
+            IEnumerable<Dice> IShipStats.Weapons => Weapons;
+        }
+
+        [Test]
+        public void BasicCombatTest()
+        {
+            var result = Combat.AttackerWin(
+                new[] { (blueprint: new TestShip { Initiative = 1, Weapons = new Dice[] { Dices.YellowDice }, Computers = 0, Shields = 0, Hulls = 0 } as IShipStats, count: 1) }, 
+                new[] { (blueprint: new TestShip { Initiative = 1, Weapons = new Dice[] { }, Computers = 0, Shields = 0, Hulls = 0 } as IShipStats, count: 1) });
+
+            Assert.IsTrue(result);
+        }
+    }
+}
