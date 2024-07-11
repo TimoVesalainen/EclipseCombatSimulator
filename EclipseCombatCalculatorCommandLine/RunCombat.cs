45e220344f6d1545b116170d4737560d91455b9e
Timo Vesalainen <timovesalainen@hotmail.com>
2024-07-11 22:56:01 +0300

    Extracted code to own helper

--- EclipseCombatCalculatorCommandLine/RunCombat.cs
+++ EclipseCombatCalculatorCommandLine/RunCombat.cs
@@ -22,6 +22,52 @@ namespace EclipseCombatCalculatorCommandLine
             return $"Ship {ship.Blueprint.Name} amount {ship.Count} damage {ship.Damage}";
         }
 
+        private static IEnumerable<(ICombatShip, IEnumerable<IDiceFace>)> PlayerDistribution(
+            ICombatShip attacker, IEnumerable<ICombatShip> targets, IEnumerable<IDiceFace> diceResult)
+        {
+            Console.WriteLine("Your dice are: {0}", string.Join(", ", diceResult.Select(PrintDiceFace)));
+
+            var mayHitDice = diceResult.Where(dice => dice is not Miss && targets.Any(target => attacker.Blueprint.CanHit(target.Blueprint, dice)));
+            if (!mayHitDice.Any())
+            {
+                Console.WriteLine("No possible hits");
+                return [];
+            }
+
+            Console.WriteLine("Please choose how to assign them. Possible targets are:");
+            Console.WriteLine(string.Join("\n", targets.Zip(Options).Select((pair) => $"{pair.Second}) {pair.First.Count} {pair.First.Blueprint.Name}{(pair.First.Count > 1 ? "s" : "")}")));
+
+            List<(IDiceFace, ICombatShip)> results = [];
+            var targetArray = targets.ToArray();
+            var length = targetArray.Length;
+            foreach (var dice in mayHitDice)
+            {
+                Console.Write("{0} => ", PrintDiceFace(dice));
+                char? input = null;
+                while (input == null)
+                {
+                    var read = Console.ReadLine();
+                    if (read == null || read.Length == 0)
+                    {
+                        Console.WriteLine("Received empty input. Let's try that again, shall we?");
+                        continue;
+                    }
+                    var index = Options.IndexOf(read[0]);
+                    if (index < 0 || index >= length)
+                    {
+                        Console.WriteLine("Invalid input. Let's try that again, shall we?");
+                        continue;
+                    }
+                    input = read[0];
+                }
+                var ship = targetArray[Options.IndexOf(input ?? ' ')];
+                results.Add((dice, ship));
+            }
+
+            return results.GroupBy(resultPair => resultPair.Item2, resultPair => resultPair.Item1)
+                .Select(group => (group.Key, group.Where(dice => attacker.Blueprint.CanHit(group.Key.Blueprint, dice))));
+        }
+
         const string Options = "abcd";
 
         public static async Task Run(Options options)
@@ -43,49 +89,10 @@ namespace EclipseCombatCalculatorCommandLine
                     }
                     return aiAssignment;
                 }
-
-
-                Console.WriteLine("Your dice are: {0}", string.Join(", ", diceResult.Select(PrintDiceFace)));
-
-                var mayHitDice = diceResult.Where(dice => dice is not Miss && targets.Any(target => attacker.Blueprint.CanHit(target.Blueprint, dice)));
-                if (!mayHitDice.Any())
+                else
                 {
-                    Console.WriteLine("No possible hits");
-                    return [];
+                    return PlayerDistribution(attacker, targets, diceResult);
                 }
-
-                Console.WriteLine("Please choose how to assign them. Possible targets are:");
-                Console.WriteLine(string.Join("\n", targets.Zip(Options).Select((pair) => $"{pair.Second}) {pair.First.Count} {pair.First.Blueprint.Name}{(pair.First.Count > 1 ? "s" : "")}")));
-
-                List<(IDiceFace, ICombatShip)> results = [];
-                var targetArray = targets.ToArray();
-                var length = targetArray.Length;
-                foreach (var dice in mayHitDice)
-                {
-                    Console.Write("{0} => ", PrintDiceFace(dice));
-                    char? input = null;
-                    while (input == null)
-                    {
-                        var read = Console.ReadLine();
-                        if (read == null || read.Length == 0)
-                        {
-                            Console.WriteLine("Received empty input. Let's try that again, shall we?");
-                            continue;
-                        }
-                        var index = Options.IndexOf(read[0]);
-                        if (index < 0 || index >= length)
-                        {
-                            Console.WriteLine("Invalid input. Let's try that again, shall we?");
-                            continue;
-                        }
-                        input = read[0];
-                    }
-                    var ship = targetArray[Options.IndexOf(input ?? ' ')];
-                    results.Add((dice, ship));
-                }
-
-                return results.GroupBy(resultPair => resultPair.Item2, resultPair => resultPair.Item1)
-                    .Select(group => (group.Key, group.Where(dice => attacker.Blueprint.CanHit(group.Key.Blueprint, dice))));
             }
 
             var attacker = GetBlueprints(options.Attacker).Zip(options.AttackerShipCounts).Where(x => x.First != null);
