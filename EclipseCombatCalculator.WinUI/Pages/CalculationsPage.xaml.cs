82c30c754a60c790332f7e496ccadab2f228aaf6
Timo Vesalainen <timovesalainen@hotmail.com>
2024-07-23 12:57:12 +0300

    Updated calculations page

--- EclipseCombatCalculator.WinUI/Pages/CalculationsPage.xaml.cs
+++ EclipseCombatCalculator.WinUI/Pages/CalculationsPage.xaml.cs
@@ -5,19 +5,11 @@ using EclipseCombatCalculator.WinUI.Dialogs;
 using EclipseCombatCalculator.WinUI.ViewModel;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using Nintenlord.Collections.Foldable;
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -82,11 +74,81 @@ namespace EclipseCombatCalculator.WinUI.Pages
             }
         }
 
+        static readonly IFolder<CombatState, (int, int, int), (int, int, int)> folder = Folders.CountI<CombatState>().Combine(
+                Folders.CountI<CombatState>(x => x.AttackerWinner == true),
+                Folders.CountI<CombatState>(x => x.AttackerWinner == false),
+                (count, attacker, defender) => (count, attacker, defender));
+
         private async void Calculate_Click(object sender, RoutedEventArgs e)
         {
+            if (!double.IsFinite(SampleCountBox.Value))
+            {
+                return;
+            }
+
             var attackerAi = (AttackerAISelection.SelectedItem as AIViewModel).Implementation;
             var defenderAi = (DefenderAISelection.SelectedItem as AIViewModel).Implementation;
+            var amountToSample = (int)SampleCountBox.Value;
+            int partitionCount = Math.Min(100, amountToSample);
+            bool isPartitioned = amountToSample > 100;
 
+            ViewModel.Progress = 0;
+            ViewModel.ProgressMax = partitionCount;
+            ViewModel.ProgressVisible = Visibility.Visible;
+
+            var attackers = ViewModel.Attackers.Select(viewModel => (viewModel.Blueprint as IShipStats, viewModel.Count)).ToList();
+            var defenders = ViewModel.Defenders.Select(viewModel => (viewModel.Blueprint as IShipStats, viewModel.Count)).ToList();
+
+            int progress = 0;
+            int completed = 0;
+            void UpdateUI(CombatState state)
+            {
+                completed++;
+                var newProgress = (int)((double)completed / amountToSample * 100);
+                if (progress != newProgress)
+                {
+                    progress = newProgress;
+
+                    this.DispatcherQueue.TryEnqueue(() =>
+                    {
+                        ViewModel.Progress = newProgress;
+                    });
+                }
+            }
+
+            var states = await Task.Run(() => DoSampling(attackers, defenders, attackerAi, defenderAi, (int)amountToSample, UpdateUI));
+
+            int count = 0;
+            int attacker = 0;
+            int defender = 0;
+
+            foreach (var state in states)
+            {
+                count++;
+                if (state.AttackerWinner == true)
+                {
+                    attacker++;
+                }
+                if (state.AttackerWinner == false)
+                {
+                    defender++;
+                }
+            }
+
+            ViewModel.AttackerWin += attacker;
+            ViewModel.DefenderWin += defender;
+            ViewModel.Combats += count;
+            ViewModel.ProgressVisible = Visibility.Collapsed;
+            ViewModel.Update();
+        }
+
+        private static async Task<List<CombatState>> DoSampling(
+            IEnumerable<(IShipStats blueprint, int count)> attackers,
+            IEnumerable<(IShipStats blueprint, int count)> defenders,
+            DamageAssigner attackerAi, DamageAssigner defenderAi,
+            int amountToSample,
+            Action<CombatState> callback)
+        {
             async Task<IEnumerable<(ICombatShip, IEnumerable<IDiceFace>)>> AssignDamage(
             ICombatShip attacker, IEnumerable<ICombatShip> targets, IEnumerable<IDiceFace> diceResult)
             {
@@ -100,24 +162,27 @@ namespace EclipseCombatCalculator.WinUI.Pages
                 }
             }
 
-            async Task<(int startRetreat, int completeRetreat)> RetreatAsker(ICombatShip activeShips)
+            Task<(int startRetreat, int completeRetreat)> RetreatAsker(ICombatShip activeShips)
             {
-                return (0, 0);
+                return Task.FromResult((0, 0));
             }
 
-            CombatState result = new();
-            await foreach (var state in CombatLogic.DoCombat(
-                ViewModel.Attackers.Select(viewModel => (viewModel.Blueprint as IShipStats, viewModel.Count)),
-                ViewModel.Defenders.Select(viewModel => (viewModel.Blueprint as IShipStats, viewModel.Count)),
-                AssignDamage, RetreatAsker))
+            List<CombatState> states = [];
+            for (int i = 0; i < amountToSample; i++)
             {
-                if (state.Ended)
+                await foreach (var state in CombatLogic.DoCombat(
+                    attackers,
+                    defenders,
+                    AssignDamage, RetreatAsker))
                 {
-                    result = state;
+                    if (state.Ended)
+                    {
+                        callback(state);
+                        states.Add(state);
+                    }
                 }
             }
-
-            // TODO: Set result
+            return states;
         }
     }
 }
--- EclipseCombatCalculator.WinUI/ViewModel/CalculationsViewModel.cs
+++ EclipseCombatCalculator.WinUI/ViewModel/CalculationsViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using EclipseCombatCalculator.Library.Blueprints;
 using EclipseCombatCalculator.Library.Combat;
+using Microsoft.UI.Xaml;
 using System;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace EclipseCombatCalculator.WinUI.ViewModel
@@ -14,6 +16,37 @@ namespace EclipseCombatCalculator.WinUI.ViewModel
         public ObservableCollection<CombatShipType> Defenders { get; } = [CombatShipType.Create(Blueprint.OrionCruiser)];
         public ObservableCollection<AIViewModel> AIs { get; } = [new AIViewModel("Basic", AI.BasicAI)];
         public bool CanStartCombat => Attackers.Count > 0 && Defenders.Count > 0;
+
+        private int attackerWin;
+        public int AttackerWin
+        {
+            get => attackerWin;
+            set
+            {
+                attackerWin = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int defenderWin;
+        public int DefenderWin
+        {
+            get => defenderWin;
+            set
+            {
+                defenderWin = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private int combats;
+        public int Combats
+        {
+            get => combats;
+            set
+            {
+                combats = value;
+                NotifyPropertyChanged();
+            }
+        }
 
         private string result;
         public string Result
@@ -26,13 +59,57 @@ namespace EclipseCombatCalculator.WinUI.ViewModel
             }
         }
 
-        public CalculationsViewModel()
+        private int progress;
+        public int Progress
         {
-            Attackers.CollectionChanged += Attackers_CollectionChanged;
-            Defenders.CollectionChanged += Attackers_CollectionChanged;
+            get => progress;
+            set
+            {
+                progress = value;
+                NotifyPropertyChanged();
+            }
         }
 
-        private void Attackers_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        private int progressMax;
+        public int ProgressMax
+        {
+            get => progressMax;
+            set
+            {
+                progressMax = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public Visibility progressVisible = Visibility.Collapsed;
+        public Visibility ProgressVisible
+        {
+            get => progressVisible;
+            set
+            {
+                progressVisible = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public CalculationsViewModel()
+        {
+            Attackers.CollectionChanged += Ships_CollectionChanged;
+            Defenders.CollectionChanged += Ships_CollectionChanged;
+        }
+
+        public void Update()
+        {
+            var attackerWinPercentage = (double)attackerWin * 100 / combats;
+            var defenderWinPercentage = (double)defenderWin * 100 / combats;
+
+            Result = $"Of {combats} samples:\n" +
+                $"Attacker {attackerWin}, Defender {defenderWin}\n" +
+                $"Attacker win portion: {attackerWinPercentage}%\n" +
+                $"Defender win portion: {defenderWinPercentage}%\n";
+        }
+
+        private void Ships_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
         {
             switch (e.Action)
             {
@@ -55,7 +132,6 @@ namespace EclipseCombatCalculator.WinUI.ViewModel
                 case NotifyCollectionChangedAction.Move:
                     break;
             }
-            NotifyPropertyChanged(nameof(CanStartCombat));
         }
 
         public void Remove(CombatShipType ship)
