1fce2a44d6c1c0a7ee6a49acec6bfd04d3c400a7
Timo Vesalainen <timovesalainen@hotmail.com>
2024-08-11 21:23:50 +0300

    Deleted comment

--- EclipseCombatCalculator.WinUI/App.xaml.cs
+++ EclipseCombatCalculator.WinUI/App.xaml.cs
@@ -5,9 +5,6 @@ using System.IO;
 using System.Text.Json;
 using Windows.Storage;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
-
 namespace EclipseCombatCalculator.WinUI
 {
     /// <summary>
